ab851ed6088b57bca23e16ee6dbcb6b840d5e678
Victor Gaspar <victorgaspar1@hotmail.com>
2023-12-05 21:45:25 -0300

    ajustando link de conex'ao

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_restx  import Api,swagger
 from model.pessoa import init_app, db
 from routes.pessoa import bp
+from sql.config import DB_CONFIG
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:root@localhost:3306/pessoa',
+        SQLALCHEMY_DATABASE_URI=f'mysql+pymysql://{DB_CONFIG["user"]}:{DB_CONFIG["password"]}@{DB_CONFIG["host"]}/{DB_CONFIG["database"]}',
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
